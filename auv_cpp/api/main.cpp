ae9cba22602d929f07e445d5930d590eca40941f
IBfettuccine <annyanibblet@gmail.com>
2026-01-20 04:29:52 +0000

    motor.hpp - created motor.hpp off of motor.py - set up pigpio in CMakeLists.txt - main.cpp is for testing motor.hpp

---
+++ auv_cpp/api/main.cpp
@@ -0,0 +1,26 @@
+//script for testing the motor class
+
+#include <iostream>
+#include <pigpio.h>
+#include "motor.hpp"
+
+int main() {
+
+    if(gpioInitialise() < 0) {
+        std::cerr << "failed to initialize pigpio" << std::endl;
+        return 1;
+    }
+
+    for(int i = 0; i < 50; i++) {
+       try {
+        Motor motor(i);
+        motor.test_motor();
+        std::cout << "Motor: " << i << std::endl;
+       } catch(...) {
+        std::cout << "Skipped: " << i << std::endl;
+       }
+    }
+
+    gpioTerminate();
+    return 0;
+}
---
+++ auv_cpp/api/motor.hpp
@@ -0,0 +1,57 @@
+//motor.hpp is the header file for the motor class
+
+#pragma once
+
+//pigpio isn't needed in simulation mode
+//if it is just get rid of the ifndef & endif
+#ifndef BUILD_SIMULATION
+#include <pigpio.h>
+#endif
+
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+class Motor {
+    private:
+        int pin;
+        double speed;
+    
+    public: 
+        //hardcoded values from AUV
+        const int CENTER_PWM_RANGE = 400;
+        const int CENTER_PWM_VALUE = 1500;
+        const int MAX_SPEED = 150;
+
+        //motor constructor
+        //initialize gpio elsewhere with gpioInitialise()
+        Motor(int gpio_pin) : pin(gpio_pin), speed(0.0) { }
+
+        //set speed of motor
+        void set_speed(double new_speed) {
+            speed = new_speed;
+
+             //threshold for positive or negative speed
+            if (speed > MAX_SPEED) {
+                speed -= MAX_SPEED;
+                speed *= -1;
+            }
+
+            //conversion from received radio speed to PWM value
+            double pwm_speed = speed * (CENTER_PWM_RANGE) / MAX_SPEED + CENTER_PWM_VALUE;
+
+            //change motor speed
+            gpioServo(pin, static_cast<unsigned int>(pwm_speed));
+        }
+
+        //test the motor by setting speed values between time intervals
+        void test_motor() {
+            std::cout << ("TESTING MOTOR 2") << std::endl;
+            this->set_speed(MAX_SPEED / 3);
+            std::this_thread::sleep_for(std::chrono::seconds(5));  
+            this->set_speed(0.0);
+        }
+
+};
+
+
