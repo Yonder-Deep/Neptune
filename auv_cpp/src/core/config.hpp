457bce50cd866e960b388f8badb019f5f81b3a52
SuhaanCoding <suhaankhurana@gmail.com>
2026-02-11 23:33:36 -0800

    motor + network config

---
+++ auv_cpp/src/core/config.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <string>
+
+struct MotorConfig {
+    int motor1pin = 4;  // From the python code
+    int motor2pin = 11;
+    int motor3pin = 18;
+    int motor4pin = 24;
+};
+struct NetworkConfig {
+    std::string IP = "";
+    int port = 8080;
+};
+
+struct Config {
+    MotorConfig motors;
+    SystemConfig system;
+
+    static Config load(const std::string& path) {
+        Config cfg;
+        return cfg;
+    }
+};
