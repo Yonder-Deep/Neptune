42b5d7df6ab73da845295891fade2d0ba526dab1
WilyHyperion <noahsmall06@gmail.com>
2026-05-06 23:24:04 +0000

    Run correct linter and add targets

--- src/tests/thruster.cc
+++ src/tests/thruster.cc
@@ -4,8 +4,8 @@
 class ThrusterTest :  public Thruster {
 public:
     // Use the protected constructor in the fixture's initializer list
-    ThrusterTest() : Thruster() {} 
-    
+    ThrusterTest() : Thruster() {}
+
 };
 
 TEST(Thruster, DutyCycle){
@@ -14,7 +14,7 @@ TEST(Thruster, DutyCycle){
     thruster->cycle = 50;
     thruster->speed = 0.0;
     EXPECT_FLOAT_EQ(7.5f, thruster->getDutyCycle());
-    
+
 }
 
 TEST(Thruster, UnarmedFailure){
