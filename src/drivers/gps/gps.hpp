ce1bf9a135b4d6e87cabfba3601257e2f2b60136
WilyHyperion <noahsmall06@gmail.com>
2026-05-19 22:47:31 +0000

    add comments

--- src/drivers/gps/gps.hpp
+++ src/drivers/gps/gps.hpp
@@ -2,15 +2,17 @@
 #include "../../types/GPSCoordinate.hpp"
 #include <iostream>
 /**
+ * An abstract class that represents a gps that can gather cordinate location
  */
 class GPS {
 public:
+
   virtual bool await_lock(int interval, int tries) {
     std::cout << "Dont call this" << std::endl;
-    return false;
+    throw std::logic_error("Tried to use a gps parent class");
   }
-  virtual GPSCoordinate *location() {
+  virtual GPSCoordinate location() {
     std::cout << "Dont call this" << std::endl;
-    return nullptr;
+    throw std::logic_error("Tried to use a gps parent class");
   }
 };
--- src/drivers/gps/hardware_gps.hpp
+++ src/drivers/gps/hardware_gps.hpp
@@ -1,16 +1,28 @@
+#pragma once
 #include "gps.hpp"
 
 #include "../../types/GPSCoordinate.hpp"
 #include <iostream>
 #include <libgpsmm.h>
 #include <string>
+#include <stdexcept>
+
 /**
  * Note - its likely that only 1 gps can function at a time
+ * Reprsents a GPS attached by usb to the pi. Uses the gpsd daemon to gather information
  */
 class HardwareGPS : public GPS {
 public:
   ~HardwareGPS() { delete this->gps_rec; }
-  GPSCoordinate *location() override {
+
+
+  /**
+   * Get the current GPS location once a valid fix is obtained.
+   * No alt is gotten.
+   * Maybe block for awhile
+   * @return GPSCoordinate containing latitude and longitude.
+   */
+  GPSCoordinate location() override {
     while (true) {
       struct gps_data_t *data;
 
@@ -21,17 +33,17 @@ public:
       if ((data = this->gps_rec->read()) == NULL) {
         std::cerr << "Read error." << std::endl;
         // TODO - what to do here?
-        return nullptr;
+          throw std::runtime_error("Failed to read gps");
       } else {
         if (data->fix.mode >= MODE_2D) {
           // Connection gotten
-          GPSCoordinate *out = new GPSCoordinate();
-          out->latitude = data->fix.latitude;
-          out->longitude = data->fix.longitude;
-
+          GPSCoordinate out = GPSCoordinate();
+          out.latitude = data->fix.latitude;
+          out.longitude = data->fix.longitude;
           return out;
         } else {
-          std::cout << "No gps fix" << std::endl;
+          //TODO - order
+          std::cout << "No gps fix yet" << std::endl;
         }
       }
     }
@@ -45,6 +57,12 @@ public:
     std::cout << "GPS loaded" << std::endl;
     this->gps_rec = new gpsmm("localhost", gpsd_port);
   }
+  /**
+   * Awaits a gps lock. Note that a lock could be lost if the gps is blocked again, in which case the next location call will re-obtain the lock
+   * @param interval Wait time between tries
+   * @param tries Number of times to try before returning false
+   * @returns the fix status
+   */
   bool await_lock(int interval, int tries) override {
     if (this->gps_rec->stream(WATCH_ENABLE | WATCH_JSON) == NULL) {
       std::cerr << "No GPSD running." << std::endl;
